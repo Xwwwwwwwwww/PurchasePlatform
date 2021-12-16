99965eec94a7b1ffb10d01918fdfd71257134a4a
Baibingtao <805453385@qq.com>
2021-12-16 13:31:26 +0800

    平台管理员登陆。

--- src/main/java/com/xwwwww/purchaseplatform/service/login/LoginServiceImpl.java
+++ src/main/java/com/xwwwww/purchaseplatform/service/login/LoginServiceImpl.java
@@ -1,6 +1,8 @@
 package com.xwwwww.purchaseplatform.service.login;
 
+import com.xwwwww.purchaseplatform.entity.administration.administrator.PlatformAdministrator;
 import com.xwwwww.purchaseplatform.entity.shopping.customer.Customer;
+import com.xwwwww.purchaseplatform.mapper.administration.administrator.PlatformAdministratorMapper;
 import com.xwwwww.purchaseplatform.mapper.shopping.customer.CustomerMapper;
 import com.xwwwww.purchaseplatform.utils.result.Result;
 import com.xwwwww.purchaseplatform.utils.result.ResultCode;
@@ -12,11 +14,19 @@ import org.springframework.stereotype.Service;
 public class LoginServiceImpl implements LoginService {
     @Autowired
     CustomerMapper customerMapper;
-
+    @Autowired
+    PlatformAdministratorMapper platformAdministratorMapper;
     @Override
     public Result login(Customer customer) throws Exception{
         if(customerMapper.login(customer)!=null)
             return Result.SUCCESS(AesUtils.encrypt(customer.getPhoneNumber()));
+        else
+            return new Result(ResultCode.USER_LOGIN_ERROR,"error");
+    }
+    @Override
+    public Result login(PlatformAdministrator platformAdministrator) throws Exception{
+        if(platformAdministratorMapper.login(platformAdministrator)!=null)
+            return Result.SUCCESS(AesUtils.encrypt(platformAdministrator.getUserName()));
         else
             return new Result(ResultCode.USER_LOGIN_ERROR,"error");
     }
