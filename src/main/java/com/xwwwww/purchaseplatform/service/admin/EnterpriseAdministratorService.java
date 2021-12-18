c858e68a9098f38cc9eaeb9663509461c011fce3
李庚润 <2078492396@qq.com>
2021-12-18 21:33:51 +0800

    EnterpriseAdministratorController

---
+++ src/main/java/com/xwwwww/purchaseplatform/service/admin/EnterpriseAdministratorService.java
@@ -0,0 +1,16 @@
+package com.xwwwww.purchaseplatform.service.admin;
+
+import com.xwwwww.purchaseplatform.entity.administration.administrator.EnterpriseAdministrator;
+import com.xwwwww.purchaseplatform.utils.result.Result;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface EnterpriseAdministratorService {
+    Result getEnterpriseAdminById(int id) throws Exception;
+
+    Result insertEnterpriseAdmin(EnterpriseAdministrator enterpriseAdministrator) throws Exception;
+
+    Result deleteEnterAdmin(int id) throws Exception;
+
+    Result updateEnterAdmin(EnterpriseAdministrator enterpriseAdministrator) throws Exception;
+}
